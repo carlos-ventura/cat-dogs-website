e8314e1e8db63a2a5b1193b2c5f4b395bf48a80f
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-24 19:09:58 +0100

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-// Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
-// Step 2: Define your component
-const IndexPage = () => {
+const IndexPage = (): JSX.Element => {
   return (
     <main>
       <h1>Cats and Dogs Website</h1>
-      <button>Cat facts</button>
-      <button>Dog pictures</button>
+      <Link to="/cats">Cat facts</Link>
+      <br/><br/>
+      <Link to="/dogs">Dog pictures</Link>
     </main>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = (): JSX.Element => <title>Index page</title>
 
-// Step 3: Export your component
 export default IndexPage
