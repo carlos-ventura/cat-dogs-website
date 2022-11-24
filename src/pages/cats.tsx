106820b21305b22bd2ce5d7aff39f67b7be77c1b
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-24 19:13:10 +0100

    Update cat page to fetch random facts and save on state

--- src/pages/cats.tsx
+++ src/pages/cats.tsx
@@ -5,12 +5,20 @@ import { useState } from 'react'
 const CatPage = (): JSX.Element => {
   const [catFacts, setCatFacts] = useState<string []>([])
 
+  async function fetchData (): Promise<void> {
+    const url = 'https://catfact.ninja/fact'
+    const response = await fetch(url)
+    const fact = await response.json()
+    setCatFacts((prevCatFacts) => ([...prevCatFacts, fact.fact]))
+  }
 
   return (
     <main>
       <h1>Cats page</h1>
       <Link to="/">Back to Home</Link>
       <br/><br/>
+      <button onClick={() => { void fetchData() }}>Generate new fact</button>
+      {catFacts.map((fact, index) => <p key={`fact-${index}`}>{index}. {fact}</p>)}
     </main>
   )
 }
