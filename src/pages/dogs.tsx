a7ea4b455f4389960ae03446ae5e0bf03ae89216
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-24 19:52:24 +0100

    Add logic to dogs

--- src/pages/dogs.tsx
+++ src/pages/dogs.tsx
@@ -3,11 +3,48 @@ import { Link } from 'gatsby'
 import dogURLs from '../data/DogURLs'
 import { useState } from 'react'
 
+const randomIntFromInterval = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min)
+
+const isPrime = (n: number): boolean => {
+  for (let i = 2; i < n; i++) { if (n % i === 0) return false }
+  return n > 1
+}
+
+const allOnesBinary = (n: number): boolean => {
+  const binary = n.toString(2)
+  return binary.length >= 4 && !binary.includes('0')
+}
 
 const DogsPage = (): JSX.Element => {
+  const [dogIndex, setDogIndex] = useState<number | undefined>()
   const [inputNumber, setInputNumber] = useState<number>()
+  const [cycle, setCycle] = useState(false)
+
+  const dogIndexLogic = (n: number): number => {
+    if (n < 0) {
+      return 0
+    } else if (allOnesBinary(n)) {
+      return 10
+    } else if (n > 100) {
+      return 9
+    } else if (isPrime(n)) {
+      return randomIntFromInterval(1, 3)
+    } else if (n % 5 === 0) {
+      setCycle(!cycle)
+      if (cycle) {
+        return 5
+      } else {
+        return 4
+      }
+    } else {
+      return Number(process.env.STATIC_DOG)
+    }
+  }
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault()
+    const result = dogIndexLogic(inputNumber ?? 0)
+    setDogIndex(result)
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
