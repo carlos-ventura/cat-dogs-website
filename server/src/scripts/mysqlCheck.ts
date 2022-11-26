c2265aba3ac0b101486150d5937216521f23dd95
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-26 01:32:37 +0100

    Fix store procedure index error

--- server/src/scripts/mysqlCheck.ts
+++ server/src/scripts/mysqlCheck.ts
@@ -6,7 +6,7 @@ const simpleLogicProcedure =
  'begin\n' +
  'declare indexNumber int;\n' +
  'if (inputNumber < 0 ) then set indexNumber = 0;\n' +
- 'elseif (inputNumber > 100) then set indexNumber = 10;\n' +
+ 'elseif (inputNumber > 100) then set indexNumber = 9;\n' +
  'end if;\n' +
  'select indexNumber;\n' +
  'end'
