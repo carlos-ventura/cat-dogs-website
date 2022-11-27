c694b7a9ac5f4ba6ed90291709bcaea6ca8aeced
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-27 22:43:37 +0100

    Refactor starting script to handle good and bad connection to the database

--- server/src/scripts/mysqlCheck.ts
+++ server/src/scripts/mysqlCheck.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const mysqlCheck = require('mysql2/promise')
+const mysqlCheckPromise = require('mysql2/promise')
+require('dotenv').config()
 
 const simpleLogicProcedure =
 'CREATE procedure if not exists doglogic.dogIndexLogic(IN inputNumber int)\n' +
@@ -13,10 +14,31 @@ const simpleLogicProcedure =
 
 async function main (): Promise<void> {
   try {
-    const connectionCheck = await mysqlCheck.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'password'
+    // wait for database setup
+    if (String(process.env.DOCKER) === 'true') {
+      console.log('Waiting for database setup in Docker...')
+      await new Promise(resolve => setTimeout(resolve, 30000))
+    }
+
+    // importing mysql2/promise: if no exception programs hangs
+    // importing mysql2: if exception thrown, can't catch it
+    // Solution try mysql2/promise, if hangs change to mysql2
+    let connectionCheck = await mysqlCheckPromise.createConnection({
+      host: String(process.env.HOST),
+      user: String(process.env.USER),
+      password: String(process.env.PASSWORD)
+    })
+      .then((conn: { end: () => void, destroy: () => void }) => {
+        conn.end()
+        conn.destroy()
+      })
+      .catch((err: any) => { throw err })
+
+    const mysqlCheck = require('mysql2')
+    connectionCheck = await mysqlCheck.createConnection({
+      host: String(process.env.HOST),
+      user: String(process.env.USER),
+      password: String(process.env.PASSWORD)
     })
 
     connectionCheck.connect(function (err: any) {
